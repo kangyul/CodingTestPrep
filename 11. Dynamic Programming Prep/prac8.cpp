1d9be83da338ca15baf775a5f15881f772258b93
kangyul <yulkang2019@gmail.com>
2021-09-13 12:51:39 -0700

    Modify prac8

--- 11. Dynamic Programming Prep/prac8.cpp
+++ 11. Dynamic Programming Prep/prac8.cpp
@@ -4,39 +4,7 @@ using namespace std;
 
 int main(void) {
 
-    int N;
-    cin >> N;
-
-    for(int i=0; i<N; i++) {
-        string str;
-        cin >> str;
-
-        int l = 0, r = str.size()-1;
-        bool flag = true;
-        int chance=0;
-
-        for(int j=0; j<str.size()/2; j++) {
-            if(str[l] != str[r]) {
-                flag = false;
-                if(chance) { // 1
-
-                } else { // 2
-                    chance++;
-                    break;
-                }
-            }
-            l++;
-            r--;
-        }
-
-        if(flag) {
-            cout << '0' << '\n';
-        } else {
-            if(chance) {
-
-            }
-        }
-    }
+    
 
     return 0;
 }
