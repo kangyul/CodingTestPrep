ace0a30d8dd9850d1ffc6a3ae78fa74c67be4a8c
kangyul <kyul719@gmail.com>
2021-09-13 14:34:19 -0700

    Finish prac8.cpp

--- 11. Dynamic Programming Prep/prac8.cpp
+++ 11. Dynamic Programming Prep/prac8.cpp
@@ -2,9 +2,56 @@
 
 using namespace std;
 
+bool check(string str, int s, int e) {
+    while (s<=e) {
+        if(str[s] != str[e]) {
+            return false;
+        }
+        s++;
+        e--;
+    }
+
+    return true;
+}
+
+void solve() {
+    string str;
+    cin >> str;
+
+    int s = 0;
+    int e = str.size() - 1;
+
+    while(s <= e) {
+
+        if(str[s] != str[e]) {
+            // 유사 회문
+            if(check(str, s+1, e) || check(str, s, e-1)) {
+                cout << '1' << '\n';
+                return;
+            }
+            // 회문 x 
+            else {
+                cout << '2' << '\n';
+                return; 
+            }
+        }
+
+        s++;
+        e--;
+    }
+
+    // 회문인 경우
+    cout << '0' << '\n';
+}
+
 int main(void) {
 
-    
+    int T;
+    cin >> T;
+
+    while(T--) {
+        solve();    
+    }
 
     return 0;
 }
