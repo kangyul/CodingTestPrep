e8b2ecf2226503892be7d38ff88c46852b9758f3
kangyul <kyul719@gmail.com>
2021-09-13 15:01:05 -0700

    Solving prac9.cpp

--- 11. Dynamic Programming Prep/prac9.cpp
+++ 11. Dynamic Programming Prep/prac9.cpp
@@ -2,9 +2,31 @@
 
 using namespace std;
 
+int arr[100004];
+
 int main(void) {
 
     ios::sync_with_stdio(0); cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    for(int i=0; i<N; i++) {
+        int x;
+        cin >> x;
+        arr[i] = x;
+    }
+
+    sort(arr, arr+N);
+
+    int s = 0;
+    int e = N-1;
+    
+    while(s<=e) {
+    
+        
+
+    }
 
     return 0;
 }
